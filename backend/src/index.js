76ec72e5723bae9937915668e920e48c54535fa7
shubham <shubham992284@gmail.com>
2025-06-22 12:06:15 +0530

    tank stack installed with axios

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,17 @@ import userRoute from '../src/routes/user.route.js'
 import chatRoute from "../src/routes/chat.route.js"
 import {connectDb} from './lib/db.js'
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 dotenv.config();
 const port=process.env.PORT
 
 const app=express();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  }));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,11 +8,24 @@ import NotificationPage from './pages/NotificationPage'
 import CallPage from './pages/CallPage'
 import ChatPage from './pages/ChatPage'
 import toast, { Toaster } from 'react-hot-toast';
+import { useQuery } from '@tanstack/react-query'
+import { axiosInstance } from './lib/axios'
 
 function App() {
+  const {data,isLoading,error}=useQuery({
+    queryKey:['todos'],
+    queryFn: async()=>{
+      const res=await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false
+    })
+
+    console.log("data")
   return (
    <>
- <button onClick={() => toast('Here is your toast.')}>Make me a toast</button>
+ {/* <button onClick={() => toast('Here is your toast.')}>Make me a toast</button> */}
+
 <Routes>
  <Route path='/' element={<HomePage/>} />
  <Route path="/login" element={<LogInPage/>} />
