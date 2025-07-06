d6884f8d9f23f7ccf75b8bc9f1f59267928e052a
shubham <shubham992284@gmail.com>
2025-07-06 15:45:11 +0530

    custom hook created for auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,21 +8,11 @@ import NotificationPage from './pages/NotificationPage'
 import CallPage from './pages/CallPage'
 import ChatPage from './pages/ChatPage'
 import toast, { Toaster } from 'react-hot-toast';
-import { useQuery } from '@tanstack/react-query'
-import { axiosInstance } from './lib/axios'
 import { Navigate } from 'react-router'
+import useAuthUser from './hooks/useAuthUser'
 
 function App() {
-  const {data:authData,isLoading,error}=useQuery({
-    queryKey:['authUser'],
-    queryFn: async()=>{
-      const res=await axiosInstance.get("/auth/me");
-      return res.data;
-    },
-    retry: false
-    })
-
-   const authUser= authData?.user;
+  const {isLoading,authUser}=useAuthUser()
   return (
    <>
  {/* <button onClick={() => toast('Here is your toast.')}>Make me a toast</button> */}
